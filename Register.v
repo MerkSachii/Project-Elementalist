0f1cd224d52452a4f9e80560b24abe1e54f50bc3
Allendale <natoallendale@gmail.com>
2017-01-26 23:22:42 +0800

    Add project variables in register file

--- Register.v
+++ Register.v
@@ -1,6 +1,21 @@
 module register_file(
-elk,
-nrst,
-rd_addrA, rd_addrB, wr_addr, wr_en,
-wr_data, rd_dataA, rd_dataB
+  wr_en, wr_addr, wr_data,
+  rd_addrA, rd_addrB,
+  rd_dataA, rd_dataB,
+  elk,
+  nrst
 );
+
+// Input port declaration
+input [4:0]   rd_addrA;
+input [4:0]   rd_addrB;
+input [4:0]   wr_addr;
+input [31:0]  wr_data;
+input         wr_en;
+input         elk;
+input         nrst;
+// Output port declaration
+output [31:0] rd_dataA;
+output [31:0] rd_dataB;
+
+endmodule
