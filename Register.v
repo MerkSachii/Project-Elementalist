f191b0dc49d2f0c5b592c51ba08359b5bcbd9b2c
Mark Sanchez <dlsu.sachii@gmail.com>
2017-01-28 12:18:03 +0800

    Addded feature: Prevent processor from writing to Register 0

--- Register.v
+++ Register.v
@@ -61,6 +61,8 @@ end
 always @(posedge elk)
 begin
   if(wr_en == 1)
+    if(wr_addr == 0)
+      $display("That is not allowed");
     Registers[wr_addr] = wr_data; // for testing only
 
   if(nrst == 1)
