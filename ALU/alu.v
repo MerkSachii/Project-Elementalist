47a334301be08fff40abaf6bdbfd67fb612a4b94
Allendale <natoallendale@gmail.com>
2017-01-28 23:43:13 +0800

    add alu

--- ALU/alu.v
+++ ALU/alu.v
@@ -1,20 +1,70 @@
 module alu(
-	opA, opB, 
-	sel, 
-	res, 
+	opA, opB,
+	sel,
+	res,
 	z, c, v
 );
 
-input reg [31:0] opA;
-input reg [31:0] opB;
-input reg [2:0] sel;
-output reg [31:0] res;
-output wire z, c, v;
+	input [31:0] opA;
+	input [31:0] opB;
+	input [2:0] sel;
+	output reg [31:0] res;
+	output reg z, c, v;
 
-integer ctr;
+	integer ctr;
 
-initial begin
-	
-end
+	initial begin
+	end
+
+	always @(*) begin
+
+		z = 0;
+		c = 0;
+		v = 0;
+		res = 32'h00000000;
+		// Addition
+		if(sel == 3'b000) begin
+			{c, res} = opA + opB;
+			$display("%b", res);
+			if(res == 32'h00000000)
+				z = 1;
+			if((opA[31] == opB[31]) && res[31] == ~opA[31])
+				v = 1;
+		end
+		// Subtraction
+		else if(sel == 3'b001) begin
+			{c, res} = opA - opB;
+			$display("%b", res);
+			if(res == 32'h00000000)
+				z = 1;
+			if((~opA[31] == opB[31]) && res[31] == ~opA[31])
+				v = 1;
+		end
+		else if(sel == 3'b010) begin
+			res = opA & opB;
+			$display("%b", res);
+			if(res == 32'h00000000)
+				z = 1;
+		end
+		else if(sel == 3'b011) begin
+			res = opA | opB;
+			$display("%b", res);
+			if(res == 32'h00000000)
+				z = 1;
+		end
+		else if(sel == 3'b100) begin
+			res = ~opA;
+			$display("%b", res);
+			if(res == 32'h00000000)
+				z = 1;
+		end
+		else begin
+			$display("Invalid 'sel' value");
+		end
+
+		$display("Zero Flag: %b", z);
+		$display("Carry Flag: %b", c);
+		$display("Overflow Flag: %b", v);
+	end
 
 endmodule
--- ALU/alu_tb.v
+++ ALU/alu_tb.v
@@ -3,59 +3,79 @@ module alu_tb();
 	reg[31:0] opA;
 	reg[31:0] opB;
 	reg[2:0] sel;
-	
+
 	wire[31:0] res;
 	wire z;
 	wire c;
 	wire v;
-	
+
 initial begin
 	$dumpfile("alu.vcd");
 	$dumpvars(0, opA, opB, sel, res, z, c, v);
-	
+
+
 	#5
 	sel = 3'b000;
-	opA = 32'h90000000;
-	opB = 32'h90000000;
-	
+	opA = 32'h8FFFFFFF;
+	opB = 32'h8FFFFFFF;
+
+	#5
+	sel = 3'b001;
+	opA = 32'h0FFFFFFF;
+	opB = 32'h8FFFFFFF;
+
+	#5
+	sel = 3'b000;
+	opA = 32'hFFFFFFFF;
+	opB = 32'h00000004;
+
+	#5
+	sel = 3'b010;
+	opA = 32'hFFFFFFFF;
+	opB = 32'h55555555;
+
 	#5
 	sel = 3'b000;
 	opA = 32'h40000000;
 	opB = 32'h40000000;
-	
+
 	#5
 	sel = 3'b000;
 	opA = 32'h00000000;
 	opB = 32'h00000000;
-	
+
 	#5
 	sel = 3'b001;
 	opA = 32'h80000000;
 	opB = 32'h70000000;
-	
+
 	#5
 	sel = 3'b001;
 	opA = 32'h00000000;
 	opB = 32'hffffffff;
-	
+
 	#5
 	sel = 3'b001;
 	opA = 32'h00000006;
 	opB = 32'h00000001;
-	
+
 	#5
 	sel = 3'b001;
 	opA = 32'h00000001;
 	opB = 32'h00000001;
-	
+
 	#5
 	sel = 3'b001;
 	opA = 32'h00000000;
 	opB = 32'h00000001;
-	
-	
+
+	#5
+	sel = 3'b110;
+	opA = 32'h00000000;
+	opB = 32'h00000001;
+
 	#20
-	
+
 	$finish;
 end
 
