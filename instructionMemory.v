4d3bfce1419b18f6ac8ecded5313e97467effeaa
Allendale Nato <natoallendale@gmail.com>
2017-02-13 01:06:51 +0800

    Fix syntax errors in instructionMem

--- instructionMemory.v
+++ instructionMemory.v
@@ -4,23 +4,23 @@ input [31:0] pc;
 output [31:0] inst;
 reg [31:0] inst;
 
-reg [31:0] memdata [127:0]
+reg [31:0] memdata [127:0];
 
 initial begin
     // ADD instruction
-    memdata[0] = 32'0000000000000000000000000010000;
+    memdata[0] = 32'b0000000000000000000000000010000;
     // LW instruction
-    memdata[1] = 32'100011;
+    memdata[1] = 32'b100011;
     // SW instruction
-    memdata[2] = 32'101011;
+    memdata[2] = 32'b101011;
     // BEQ instruction
-    memdata[3] = 32'000100;
+    memdata[3] = 32'b000100;
     // BNE instruction
-    memdata[4] = 32'000101;
+    memdata[4] = 32'b000101;
     // SUB instruction
-    memdata[5] = 32'000000;
+    memdata[5] = 32'b000000;
     // SLT instruction
-    memdata[6] = 32'000000;
+    memdata[6] = 32'b000000;
 
     end
 
